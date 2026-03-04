aa7377e3761f7074130e131ec74d5573c48d0087
Mandy Pytleski <pytleskia5362@my.uwstout.edu>
2026-03-04 13:14:26 -0600

    first creation, got exit working, started doing history but didn't quite get it working :)

--- shell.cpp
+++ shell.cpp
@@ -1,15 +1,100 @@
 #include <iostream>
 #include <string>
-#include <fstream>
+#include <vector>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <fstream>
 #include <string.h>
+#include <unistd.h>  //getcwd
+#include <sstream>
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <sys/types.h>
-#include<sys/wait.h>
-#include <stdlib.h>     /* exit, EXIT_FAILURE */
 using namespace std;
 
 //WORKED WITH Ellanor Splinter (section ---) and Abby Gottfried (section ---)
+//Shared file on GitHub
+
+/*What he talked about in class
+main(){
+vector<string>history = getHistroy("hit.txt");
+string line;
+while(true){
+if(line == "exit")
+exit(0);
+else if(line=="pwd")
+cout << pwd()<<"~$"
+getline(cin, line);
+history.push_back(line); //updates history file
+}
+}
+
+vector<string>getHistory(string){
+string pwd pwd(){}
+}
+
+create function to change standard output
+elseif(stdOutRed(line)>-1{}
+
+same with in
+
+cmd > File.name
+cmd < File.name
+
+elseif(hasPiping(line)>-1){
+cmd1 | cmd2 //see example 10(?)
+
+
+else(simple command) use execvp
+*/
+
+vector<string> splitString(string str){
+
+
+    vector<string> words;
+    string word;
+    for (char c : str)
+    {
+        if (c == ' ')
+        {
+            if (!word.empty())
+            {
+                words.push_back(word);
+                word.clear();
+            }
+        }
+        else
+        {
+            word += c;
+        }
+    }
+    if (!word.empty())
+    {
+        words.push_back(word);
+    }
+    return words;
+}
+
+void createArray(const vector<string>& words, char* command[]) {
+
+    int n = words.size();
+
+    for (int i = 0; i < n; i++) {
+        command[i] = (char*) words[i].c_str();
+    }
+
+    command[n] = NULL;  
+}
+
+void addToHistory(string line){
+	string fileName = "history.txt";
+	int outFd = open(fileName.c_str(), O_WRONLY | O_CREAT, 0666);
+	int orgOut = dup(1);
+	dup2(outFd, 1);
+	cout<<line<<endl;
+	dup2(orgOut, 1);
+}
 
 int main(){
 //creating two pipes
@@ -24,6 +109,25 @@ int main(){
 	{
 		cout << "Cannot create the pipe\n";
 		exit(2);
+	}
+
+	string input = "";
+	vector<string> words;
+	
+	while(input != "exit" && input != "EXIT"){
+		cout << "$";
+		getline(cin, input);
+
+		//print to history
+		addToHistory(input);
+		
+		//splits into array of words
+		words = splitString(input);
+		int n = words.size();
+
+		//initialize c_string array
+		char* command[n+1]; 
+		createArray(words, command);
 	}
 
     
